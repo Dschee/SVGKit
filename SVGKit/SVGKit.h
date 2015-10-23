0b9ae8af5f4d8597c1a97f6053e3d1766b508e1f
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-10-23 09:58:13 +0200

    added Framework target + configured settings of Framework target (file inclusions, version etc.)

---
+++ SVGKit/SVGKit.h
@@ -0,0 +1,19 @@
+//
+//  SVGKit.h
+//  SVGKit
+//
+//  Created by Cihat Gündüz on 23.10.15.
+//  Copyright © 2015 na. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVGKit.
+FOUNDATION_EXPORT double SVGKitVersionNumber;
+
+//! Project version string for SVGKit.
+FOUNDATION_EXPORT const unsigned char SVGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGKit/PublicHeader.h>
+
+

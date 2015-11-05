4756270197f510a4de1bee05000e6c3c3fc14b83
Cihat Gündüz <CihatGuenduez@posteo.de>
2015-11-05 15:32:42 +0100

    integrated CocoaLumberjack via cocoapods

---
+++ Pods/Target Support Files/Pods-SVGKit-iOS/Pods-SVGKit-iOS-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SVGKit_iOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SVGKit_iOSVersionString[];
+
---
+++ Pods/Target Support Files/Pods-SVGKitFramework-tvOS-CocoaLumberjack/Pods-SVGKitFramework-tvOS-CocoaLumberjack-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SVGKitFramework_tvOS_CocoaLumberjack : NSObject
+@end
+@implementation PodsDummy_Pods_SVGKitFramework_tvOS_CocoaLumberjack
+@end
